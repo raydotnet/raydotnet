9cd21b5e66fa4399b1cd0a98563ab30e5eb57794
ray burnette <rayburnette83@gmail.com>
2026-03-31 21:44:14 -0400

    moving everything in

---
+++ homepage.js
@@ -0,0 +1,15 @@
+function openAbout() {
+window.open("about/about.html", "_self")
+}
+
+function openProjects() {
+window.open("projects/projects.html", "_self")
+}
+
+function openSocials() {
+window.open("socials/socials.html", "_self")
+}
+
+function openConnect() {
+window.open("connect/connect.html", "_self")
+}
